b27c6878de225ea4d336128402dba75a19b42f21
Faraz Mahmood <faraz.m4765@gmail.com>
2022-11-09 19:01:26 +0500

    link fix

--- src/Components/SourceFiles/url.js
+++ src/Components/SourceFiles/url.js
@@ -1,2 +1,6 @@
-const Baseurl="https://digicardapis.khannburger.com/api/"
+// const Baseurl="https://digicardapis.khannburger.com/api/"
+
+const Baseurl = "https://www.digicardapi.alphanitesofts.com/api/"
+
+
 export default Baseurl
